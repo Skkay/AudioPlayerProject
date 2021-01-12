68dba9bb5ab7bb77ca5592929e29dd53cf4c18a4
Skkay <skkay.thomas@gmail.com>
2021-01-12 16:41:30 +0100

    Adding Identity (Register, Login, Logout)

---
+++ AudioPlayerProject/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AudioPlayerProject.Areas.Identity.Data;
+using AudioPlayerProject.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AudioPlayerProject.Areas.Identity.IdentityHostingStartup))]
+namespace AudioPlayerProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AudioPlayerProjectContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AudioPlayerProjectContextConnection")));
+
+                services.AddDefaultIdentity<AudioPlayerProjectUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AudioPlayerProjectContext>();
+            });
+        }
+    }
+}
