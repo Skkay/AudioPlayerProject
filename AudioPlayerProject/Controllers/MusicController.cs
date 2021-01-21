e4c193dfb16a19814833519cb520b247b70ac789
Skkay <skkay.thomas@gmail.com>
2021-01-21 14:20:23 +0100

    Upload file to server

---
+++ AudioPlayerProject/Controllers/MusicController.cs
@@ -0,0 +1,39 @@
+﻿using AudioPlayerProject.Models.Music;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AudioPlayerProject.Controllers
+{
+    public class MusicController : Controller
+    {
+        private readonly IHostingEnvironment hostingEnvironment;
+        public MusicController(IHostingEnvironment environment)
+        {
+            hostingEnvironment = environment;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new Music());
+        }
+
+        [HttpPost]
+        public IActionResult Index(Music music)
+        {
+            if (music.MusicFile != null)
+            {
+                var fileName = Path.GetFileName(music.MusicFile.FileName);
+                var uploads = Path.Combine(hostingEnvironment.WebRootPath, "uploads");
+                var filePath = Path.Combine(uploads, fileName);
+
+                music.MusicFile.CopyTo(new FileStream(filePath, FileMode.Create));
+            }
+            return View();
+        }
+    }
+}
