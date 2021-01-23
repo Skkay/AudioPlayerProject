35a9f7098a2740c1968cf2fb8782b9e31b4ffc29
Skkay <skkay.thomas@gmail.com>
2021-01-23 19:03:26 +0100

    Add playlist created to database

--- AudioPlayerProject/Controllers/PlaylistController.cs
+++ AudioPlayerProject/Controllers/PlaylistController.cs
@@ -1,16 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AudioPlayerProject.Data;
+using AudioPlayerProject.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace AudioPlayerProject.Controllers
 {
     public class PlaylistController : Controller
     {
+        private PlaylistContext context;
+
+        public PlaylistController(PlaylistContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Playlist playlist)
+        {
+            try
+            {
+                string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                playlist.AudioPlayerProjectUserId = userId;
+
+                context.Playlists.Add(playlist);
+                context.SaveChanges();
+
+                ModelState.Clear();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return RedirectToAction();
         }
     }
 }
