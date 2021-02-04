6fa0f168b7263ce96afd2e651d735b17ea5c60d2
Skkay <skkay.thomas@gmail.com>
2021-02-04 20:36:56 +0100

    Search bar to easily find music or playlist

---
+++ AudioPlayerProject/wwwroot/js/searchTable.js
@@ -0,0 +1,18 @@
+﻿function searchTableFunction(inputId, tableId, columnPosition) {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById(inputId);
+    filter = input.value.toUpperCase();
+    table = document.getElementById(tableId);
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[columnPosition];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
