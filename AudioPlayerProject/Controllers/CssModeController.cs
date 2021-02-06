5a8498c3e13eecd6dcb3b2935933d17a661832a2
Skkay <skkay.thomas@gmail.com>
2021-02-06 16:27:01 +0100

    Dark mode toggle

---
+++ AudioPlayerProject/Controllers/CssModeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AudioPlayerProject.Controllers
+{
+    public class CssModeController : Controller
+    {
+        public IActionResult ToggleCSS()
+        {
+            if (HttpContext.Request.Cookies["darkMode"] == "no")
+            {
+                CookieOptions options = new CookieOptions();
+                options.Expires = new DateTimeOffset(DateTime.Now.AddYears(1));
+                HttpContext.Response.Cookies.Append("darkMode", "yes", options);
+            }
+            else
+            {
+                HttpContext.Response.Cookies.Append("darkMode", "no");
+            }
+
+            return Redirect(Request.Headers["Referer"]);
+        }
+    }
+}
